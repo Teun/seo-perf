ef55dec64afaa85002ace559eef79da4acbdaa53
Teun Duynstee <github@duynstee.com>
2013-09-06 11:01:34 +0200

    basic site with image

--- web.js
+++ web.js
@@ -1,12 +1,30 @@
 var express = require("express");
+var hbs = require("hbs");
 var app = express();
 app.use(express.logger());
 
+// set the view engine to use handlebars
+app.set('view engine', 'hbs');
+app.set('views', __dirname + '/views');
+
+app.use(express.static(__dirname + '/static'));
+
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+
+  response.locals = 
+    {
+      bodyText: 'This is the text. This is the text. This is the text. This is the text. This is the text. This is the text. This is the text. This is the text. This is the text. This is the text. This is the text. This is the text. This is the text. This is the text. This is the text. ',
+      host: getHostName(request)
+    }
+  response.render('index');
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
+
+function getHostName(request){
+  return request.headers['HTTP_HOST'];
+}
